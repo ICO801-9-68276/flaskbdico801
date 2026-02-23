495847d3d2e3aee75945bc66335ef5cd76e0da6c
ICO801-9-68276 <caferr2.2005@gmail.com>
2026-02-23 14:24:11 -0600

    Clase (práctica cinépolis)

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+import forms
 
 app = Flask(__name__)
 
@@ -7,9 +8,25 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
-@app.route("/usuario")
+@app.route("/usuarios",methods=["GET","POST"])
 def usuario():
-	return render_template('usuario.html')
+    mat=0
+    nom=''
+    apa=''
+    ama=''
+    edad=0
+    email=''
+    usuarios_clas=forms.UserForm(request.form)
+    if request.method=='POST':
+        mat=usuarios_clas.matricula.data
+        nom=usuarios_clas.nombre.data
+        apa=usuarios_clas.apaterno.data
+        ama=usuarios_clas.amaterno.data
+        edad=usuarios_clas.edad.data
+        email=usuarios_clas.correo.data
+    
+    return render_template('usuarios.html',form=usuarios_clas,mat=mat,
+                           nom=nom,apa=apa,ama=ama,edad=edad,email=email)
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,10 +1,12 @@
-from wtforms import form
-from wtforms import StringField, IntegerField, EmailField, PasswordField
+from wtforms import Form
+from wtforms import StringField, IntegerField, EmailField,PasswordField
 from wtforms import validators
 
-class UserForm(form):
-    nombre = StringField('Nombre')
-    apaterno = StringField('Apaterno')
-    amaterno = StringField('Amaterno')
-    edad = IntegerField("Edad")
-    correo = EmailField('Correo')
+
+class UserForm(Form):
+    matricula=IntegerField("Matricula")
+    nombre=StringField('Nombre')
+    apaterno=StringField('Apaterno')
+    amaterno=StringField('Amaterno')
+    edad=IntegerField("Edad")
+    correo=EmailField('Correo')
