07f10ced2177851880298f86ceacd70e7d9e6500
ICO801-9-68276 <caferr2.2005@gmail.com>
2026-03-05 20:59:59 -0600

    Tercer Commit

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template,request
-import forms
-
+from flask  import Flask, render_template,request, redirect, url_for
+from flask import flash
+from flask_wtf.csrf import CSRFProtect
+from config import DevelopmentConfig
+from models import db, Alumnos
+import forms  
 app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+db.init_app(app)
+csrf = CSRFProtect(app)
 
 @app.route("/")
 @app.route("/index")
@@ -29,4 +35,9 @@ def usuario():
                            nom=nom,apa=apa,ama=ama,edad=edad,email=email)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    csrf.init_app(app)
+    with app.app_context():
+        db.create_all()
+    app.run()
+
+      
---
+++ config.py
@@ -0,0 +1,11 @@
+import os 
+from sqlalchemy import create_engine  
+
+class Config(object):
+    SECRET_KEY ="claveSecreta"
+    SESSION_COOKIE_SECURE = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://trombei:Rootwan1*@127.0.0.1/ico801'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
